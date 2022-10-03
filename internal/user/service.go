76e65bbb911e1da31071279911fa667fcf28403c
AlexanderAST <89164909109@bk.ru>
2022-10-03 18:05:49 +0300

    add mongoDB

--- internal/user/service.go
+++ internal/user/service.go
@@ -1,1 +1,15 @@
 package user
+
+import (
+	"ServerApi/pkg/logging"
+	"context"
+)
+
+type Service struct {
+	storage Storage
+	logger  *logging.Logger
+}
+
+func (s *Service) Create(ctx context.Context, dto CreateUserDTO) (u User, err error) {
+	return
+}
